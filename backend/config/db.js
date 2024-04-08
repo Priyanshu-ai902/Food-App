af269010d396c04569abc93fd85e82b3e566460b
Priyanshu-ai902 <146703385+Priyanshu-ai902@users.noreply.github.com>
2024-04-08 22:59:40 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,5 +12,3 @@ module.exports = connectDB;
 
 
 
-// jha75059 JkI0Prf9x5zLimgV
-// mongodb+srv://jha75059:JkI0Prf9x5zLimgV@cluster0.acffwr0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
