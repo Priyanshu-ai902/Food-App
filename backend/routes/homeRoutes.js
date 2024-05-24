e695776f14ba02b75e03f5ac4e2ddba67d920906
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-05-24 19:13:00 +0530

    commit changes

--- backend/routes/homeRoutes.js
+++
@@ -1,8 +0,0 @@
-const{ Router } = require("express")
-const router= Router();
-
-router.get("",async (req, res) => {
-    res.status(200).send({message:"welcome to online food-app website"});
-})
-
-module.exports=router
